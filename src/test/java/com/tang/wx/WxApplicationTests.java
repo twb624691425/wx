4c44084fadf6d7666f88e9d1355b513d4abb0b7b
twb <624691425@qq.com>
2023-10-28 13:01:30 +0800

    feat(基础框架): 初始化项目

---
+++ src/test/java/com/tang/wx/WxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tang.wx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WxApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
